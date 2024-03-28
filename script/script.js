95da2884829c20f6a0afccfa6d66183597d2f78e
Saltanova Zhanna <nedikovazhanna@gmail.com>
2024-03-28 13:56:56 +0200

    fix

---
+++ script/script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector('.burger-button').addEventListener('click', function(e) {
+      e.preventDefault();
+      document.querySelector('.burgermenu').classList.toggle('active');
+      
+    });
+    document.querySelector('.close').addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector('.burgermenu').classList.toggle('active');
+        
+      });
+  });
